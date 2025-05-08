a8f98bbd9bf2ba124b36c7b92ffa209dc469458c
MegamindDaInevitable <edwinreitu@gmail.com>
2025-05-08 18:52:49 +0200

    adding stylesheets

--- main.cpp
+++ main.cpp
@@ -7,7 +7,23 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+
     MainWindow w;
+    QString globalStyle = R"(
+    QWidget { background-color: rgba(128, 128, 128, 128); color: white; }
+    QPushButton {
+        background-color: #444;
+        color: white;
+        border-radius: 5px;
+        padding: 6px;
+    }
+    QPushButton:hover { background-color: gray; }
+    QPushButton:pressed { background-color: darkgray; }
+)";
+
+
+    w.setStyleSheet(globalStyle);
     w.showMaximized();
     w.setFixedSize(300, 400);
     w.show();
