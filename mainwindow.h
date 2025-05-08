b32deb3ed445dfc5f2ee067bb763e2ee50e54fd4
MegamindDaInevitable <edwinreitu@gmail.com>
2025-05-08 19:04:20 +0200

    memory leaks management

--- mainwindow.h
+++ mainwindow.h
@@ -1,10 +1,14 @@
 
-/*#ifndef MAINWINDOW_H
+#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
 #include <QLabel>
 #include <QPushButton>
+#include "hexadecimal.h"
+#include "binary.h"
+#include "decimal.h"
+#include "octal.h"
 
 //#include "binary.h"
 
@@ -32,52 +36,11 @@ private:
     QLabel *prompt{nullptr};
     static constexpr int ButtonCount = 5;
     QPushButton *buttons[ButtonCount] = {nullptr};
+    Binary *binaryWindow{nullptr};
+    Octal *octalWindow{nullptr};
+    Decimal *decimalWindow{nullptr};
+    Hexadecimal *hexadecimalWindow{nullptr};
 
-
-
-};
-
-#endif // MAINWINDOW_H*/
-
-
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-#include <QLabel>
-#include <QPushButton>
-#include "binary.h"
-#include "octal.h"
-#include "decimal.h"
-#include "hexadecimal.h"
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-private:
-    void geomtry();
-
-private slots:
-    void octalButtonClicked();
-    void binaryButtonClicked();
-    void decimalButtonClicked();
-    void hexadecimalButtonClicked();
-
-private:
-    QLabel *prompt{nullptr};
-    static constexpr int ButtonCount = 5;
-    QPushButton *buttons[ButtonCount] = {nullptr};
-
-    // Reusable windows to avoid memory leaks
-    Binary* binaryWindow{nullptr};
-    Octal* octalWindow{nullptr};
-    Decimal* decimalWindow{nullptr};
-    Hexadecimal* hexadecimalWindow{nullptr};
 };
 
 #endif // MAINWINDOW_H
